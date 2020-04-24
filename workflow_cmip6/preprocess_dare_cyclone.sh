e590de6ab84bc83bc5d899b6cef2804304b8dc4d
Christian Page <christian.page@cerfacs.fr>
2020-04-24 11:05:56 +0200

    Added directory check and changed from d4p to cyclone.

--- workflow_cmip6/preprocess_dare_cyclone.sh
+++ workflow_cmip6/preprocess_dare_cyclone.sh
@@ -7,13 +7,18 @@ then
   RUN_DIR=$$
 fi
 
-cd /home/mpiuser/sfs/
-mkdir -p d4p
+if [ ! -d /home/mpiuser/sfs ]
+then
+  mkdir -p /home/mpiuser/sfs
+fi
 
-cd /home/mpiuser/sfs/d4p
+cd /home/mpiuser/sfs/
+mkdir -p cyclone
+
+cd /home/mpiuser/sfs/cyclone
 mkdir -p $RUN_DIR
 
-WORKDIR="/home/mpiuser/sfs/d4p/${RUN_DIR}"
+WORKDIR="/home/mpiuser/sfs/cyclone/${RUN_DIR}"
 
 tar -cf - -C /home/mpiuser/docker . | tar -xpf - -C ${WORKDIR}/.
 
