ecd8ecce30cf30b659f29f61a0813e1a94b1aae9
Christian Page <christian.page@cerfacs.fr>
2020-05-26 10:16:13 +0200

    Changed default values for testing.

--- workflow_cmip6/tracking_master_cmip6.sh
+++ workflow_cmip6/tracking_master_cmip6.sh
@@ -111,11 +111,11 @@ zg1000="zg"
 lsm="sftlf"
 
 latmin="30.0"
-latmax="90.0"
+latmax="50.0"
 lonmin="345.0"
-lonmax="30.0"
+lonmax="10.0"
 period_start_date="20500101"
-period_end_date="20691231"
+period_end_date="20511231"
 period=${period_start_date}-${period_end_date}
 
 curdir=`pwd`
